26cee6b0ded7a7c268c6361131aa3f3c3b597b62
AsifNawaz0129 <asif.nawaz.fast@gmail.com>
2022-04-17 13:10:58 +0500

    fix naming

--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -7,6 +7,8 @@ Cypress.on("test:after:run", (test, runnable) => {
     let videoName = Cypress.spec.name
     videoName = videoName.replace('/.ts.*', '.ts')
     const videoUrl = 'videos/' + videoName + '.mp4'
-
+    console.log(videoUrl)
+    console.log('**********************************************')
+    console.log(test)
     addContext({ test }, videoUrl)
 });
