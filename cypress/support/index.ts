30544315a668290518dcaff1bedda8a2970469d9
AsifNawaz0129 <asif.nawaz.fast@gmail.com>
2022-04-17 12:59:47 +0500

    fix naming

--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -5,7 +5,7 @@ import addContext from 'mochawesome/addContext'
 Cypress.on("test:after:run", (test, runnable) => {
     
     let videoName = Cypress.spec.name
-    videoName = videoName.replace('/.js.*', '.js')
+    videoName = videoName.replace('/.ts.*', '.ts')
     const videoUrl = 'videos/' + videoName + '.mp4'
 
     addContext({ test }, videoUrl)
